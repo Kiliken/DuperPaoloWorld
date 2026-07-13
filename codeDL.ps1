2a7476d0e6fa383161c61ad5958e9fea3ccb9a00
Paolo Mariani <131953633+Kiliken@users.noreply.github.com>
2026-07-13 16:28:00 +0900

    Initial commit

---
+++ codeDL.ps1
@@ -0,0 +1,11 @@
+
+$dest = (Get-Location).Path
+
+curl.exe -L "https://github.com/Kiliken/ue5-cmdlineTools-setup/archive/refs/heads/main.tar.gz" -o "$($dest)\main.tar.gz" --progress-bar
+
+tar -xzf "$($dest)\main.tar.gz" -C "$($dest)"
+
+Move-Item "$($dest)\ue5-cmdlineTools-setup-main\*" "$($dest)" -Force
+Remove-Item "$($dest)\ue5-cmdlineTools-setup-main" -Recurse -Force
+Remove-Item "$($dest)\main.tar.gz" -Force
+Rename-Item "$($dest)\gitignore.txt" ".gitignore"
